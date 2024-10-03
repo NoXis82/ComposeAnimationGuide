35fd2a53dda82bdf242f2b89be7e803f1e8a52ad
Tumanov Vycheslav <63115274+NoXis82@users.noreply.github.com>
2024-10-03 21:30:13 +0300

    AnimatedVisibility

--- app/src/main/java/com/noxis/composeanimationguide/MainActivity.kt
+++ app/src/main/java/com/noxis/composeanimationguide/MainActivity.kt
@@ -4,13 +4,27 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.shrinkHorizontally
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideInVertically
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
 import com.noxis.composeanimationguide.ui.theme.ComposeAnimationGuideTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,28 +34,33 @@ class MainActivity : ComponentActivity() {
         setContent {
             ComposeAnimationGuideTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(innerPadding)
+                    ) {
+
+                        var visible by remember {
+                            mutableStateOf(false)
+                        }
+
+                        Button(onClick = { visible = !visible }) {
+                            Text(text = "Toggle")
+                        }
+
+// Animated visibility will eventually remove the item from the composition once the animation has finished.
+                        AnimatedVisibility(
+                            visible = visible,
+                            enter = fadeIn() + slideInHorizontally(),
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .weight(1f)
+                        ) {
+                            Box(modifier = Modifier.background(Color.Green))
+                        }
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ComposeAnimationGuideTheme {
-        Greeting("Android")
     }
 }
