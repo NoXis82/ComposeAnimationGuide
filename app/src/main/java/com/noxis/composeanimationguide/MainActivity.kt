0a4d3539caf8bf90890d5be4eec6369583898099
Tumanov Vycheslav <63115274+NoXis82@users.noreply.github.com>
2024-10-03 22:26:28 +0300

    Animating content change

--- app/src/main/java/com/noxis/composeanimationguide/MainActivity.kt
+++ app/src/main/java/com/noxis/composeanimationguide/MainActivity.kt
@@ -4,24 +4,29 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.animation.animateColor
-import androidx.compose.animation.core.RepeatMode
-import androidx.compose.animation.core.infiniteRepeatable
-import androidx.compose.animation.core.rememberInfiniteTransition
-import androidx.compose.animation.core.tween
+import androidx.compose.animation.AnimatedContent
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideOutHorizontally
+import androidx.compose.animation.togetherWith
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
 import com.noxis.composeanimationguide.ui.theme.ComposeAnimationGuideTheme
 
+@OptIn(ExperimentalAnimationApi::class)
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,26 +39,41 @@ class MainActivity : ComponentActivity() {
                             .fillMaxSize()
                             .padding(innerPadding)
                     ) {
+                        var visible by remember {
+                            mutableStateOf(false)
+                        }
 
-                        val transition = rememberInfiniteTransition(label = "")
-                        val color by transition.animateColor(
-                            initialValue = Color.Red,
-                            targetValue = Color.Green,
-                            animationSpec = infiniteRepeatable(
-                                animation = tween(2000),
-                                repeatMode = RepeatMode.Reverse
-                            ),
-                            label = "color"
-                        )
+                        Button(onClick = { visible = !visible }) {
+                            Text(text = "Toggle")
+                        }
 
-                        Box(
+                        AnimatedContent(
+                            targetState = visible,
                             modifier = Modifier
-                                .size(200.dp)
-                                .drawBehind {
-                                    drawRect(color)
+                                .fillMaxWidth()
+                                .weight(1f),
+                            content = {
+                                if (it) {
+                                    Box(modifier = Modifier.background(Color.Green))
+                                } else {
+                                    Box(modifier = Modifier.background(Color.Red))
                                 }
-                        )
+                            },
+                            transitionSpec = {
+//                                fadeIn() with fadeOut()
+//                                slideInHorizontally {
+//                                    -it
+//                                } togetherWith fadeOut()
 
+                                slideInHorizontally {
+                                    if (visible) it else -it
+                                } togetherWith slideOutHorizontally {
+                                    if (visible) -it else it
+                                }
+
+                            },
+                            label = ""
+                        )
                     }
                 }
             }
