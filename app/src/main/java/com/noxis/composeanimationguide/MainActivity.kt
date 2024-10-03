16a2e0b2005057b03c50bf88b5e7d10dc07f1f3f
Tumanov Vycheslav <63115274+NoXis82@users.noreply.github.com>
2024-10-03 21:44:27 +0300

    Animate any value

--- app/src/main/java/com/noxis/composeanimationguide/MainActivity.kt
+++ app/src/main/java/com/noxis/composeanimationguide/MainActivity.kt
@@ -5,6 +5,8 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.animation.shrinkHorizontally
@@ -16,6 +18,8 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -23,8 +27,12 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.unit.dp
 import com.noxis.composeanimationguide.ui.theme.ComposeAnimationGuideTheme
 
 class MainActivity : ComponentActivity() {
@@ -44,19 +52,26 @@ class MainActivity : ComponentActivity() {
                             mutableStateOf(false)
                         }
 
+                        val animatedAlpha by animateFloatAsState(
+                            targetValue = if (visible) 1.0f else 0f,
+                            animationSpec = tween(durationMillis = 1500),
+                            label = "alpha"
+                        )
+
                         Button(onClick = { visible = !visible }) {
                             Text(text = "Toggle")
                         }
 
-// Animated visibility will eventually remove the item from the composition once the animation has finished.
-                        AnimatedVisibility(
-                            visible = visible,
-                            enter = fadeIn() + slideInHorizontally(),
+                        Box(
                             modifier = Modifier
-                                .fillMaxWidth()
-                                .weight(1f)
+                                .size(200.dp)
+                                .graphicsLayer {
+                                    alpha = animatedAlpha
+                                }
+                                .clip(RoundedCornerShape(8.dp))
+                                .background(Color.Green)
                         ) {
-                            Box(modifier = Modifier.background(Color.Green))
+
                         }
                     }
                 }
