294afa3984f0d0c7d0c3424d6fc5ee94e38038ea
Tumanov Vycheslav <63115274+NoXis82@users.noreply.github.com>
2024-10-03 22:11:29 +0300

    Infinite animation

--- app/src/main/java/com/noxis/composeanimationguide/MainActivity.kt
+++ app/src/main/java/com/noxis/composeanimationguide/MainActivity.kt
@@ -4,28 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.animation.core.animateFloat
-import androidx.compose.animation.core.animateInt
+import androidx.compose.animation.animateColor
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
 import androidx.compose.animation.core.tween
-import androidx.compose.animation.core.updateTransition
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.unit.dp
 import com.noxis.composeanimationguide.ui.theme.ComposeAnimationGuideTheme
 
@@ -42,49 +35,25 @@ class MainActivity : ComponentActivity() {
                             .padding(innerPadding)
                     ) {
 
-                        var visible by remember {
-                            mutableStateOf(false)
-                        }
-
-                        var isRound by remember {
-                            mutableStateOf(false)
-                        }
-
-                        val transition = updateTransition(targetState = isRound, label = null)
-
-                        val borderRadius by transition.animateInt(
-                            transitionSpec = { tween(2000) },
-                            label = "border",
-                            targetValueByState = {
-                                if (it) 100 else 0
-                            })
-
-                        val animatedAlpha by transition.animateFloat(
-                            transitionSpec = { tween(1500) },
-                            label = "alpha",
-                            targetValueByState = {
-                                if (it) 1.0f else 0f
-                            }
+                        val transition = rememberInfiniteTransition(label = "")
+                        val color by transition.animateColor(
+                            initialValue = Color.Red,
+                            targetValue = Color.Green,
+                            animationSpec = infiniteRepeatable(
+                                animation = tween(2000),
+                                repeatMode = RepeatMode.Reverse
+                            ),
+                            label = "color"
                         )
-
-                        Button(onClick = {
-                            visible = !visible
-                            isRound = !isRound
-                        }) {
-                            Text(text = "Toggle")
-                        }
 
                         Box(
                             modifier = Modifier
                                 .size(200.dp)
-                                .graphicsLayer {
-                                    alpha = animatedAlpha
+                                .drawBehind {
+                                    drawRect(color)
                                 }
-                                .clip(RoundedCornerShape(borderRadius))
-                                .background(Color.Green)
-                        ) {
+                        )
 
-                        }
                     }
                 }
             }
